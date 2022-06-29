a334f4e859be98729c9ccd89e4508287a45c85bc
leeky <sleekydz86@icloud.com>
2022-06-29 12:41:50 +0900

    적용

--- JavaStudy/src/com/study/giaischool007/Ex07.java
+++ JavaStudy/src/com/study/giaischool007/Ex07.java
@@ -9,7 +9,15 @@ public class Ex07 {
 			for(int j=5; j>i; j--) {
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println("");
+		}
+		System.out.println("-------------------");
+		System.out.println("");
+		for(int i=5; i>=0; i--) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
 		}
 	}
 }
