083e1bb1ac1349de319fcd31be5255fed0709a12
leeky <sleekydz86@icloud.com>
2022-06-30 08:38:10 +0900

    마무리2

--- JavaStudy/src/com/study/giaischool007/Ex07.java
+++ JavaStudy/src/com/study/giaischool007/Ex07.java
@@ -19,5 +19,13 @@ public class Ex07 {
 			}
 			System.out.println(" ");
 		}
+		
+		//답안
+				for(int i=0; i<=4; i++) {
+					for(int j=1; j<=5-i; j++) {
+						System.out.print("*");
+					}
+					System.out.println();
+				}
 	}
 }
