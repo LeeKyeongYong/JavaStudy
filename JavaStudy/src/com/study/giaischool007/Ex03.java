8e02296bcbe07175fd6a727a233b44e63c9a5361
인공지능사관학교 <@DESKTOP-JKHMJJ5>
2022-06-29 10:29:50 +0900

    구구당 2단

---
+++ JavaStudy/src/com/study/giaischool007/Ex03.java
@@ -0,0 +1,12 @@
+package com.study.giaischool007;
+/*
+Exam035
+
+*/
+public class Ex03 {
+	public static void main(String[] args) {
+		for(int i=1; i<10; i++) {
+			System.out.println(2+" * "+i+" = "+(2*i));
+		}
+	}
+}
