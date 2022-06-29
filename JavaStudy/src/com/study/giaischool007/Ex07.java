0eeac38e89e9b19638e6a78e7c553702968511b0
leeky <sleekydz86@icloud.com>
2022-06-29 12:33:59 +0900

    적용!

---
+++ JavaStudy/src/com/study/giaischool007/Ex07.java
@@ -0,0 +1,15 @@
+package com.study.giaischool007;
+/*
+Exam037
+
+*/
+public class Ex07 {
+	public static void main(String[] args) {
+		for(int i=0; i<=5; i++) {
+			for(int j=5; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
