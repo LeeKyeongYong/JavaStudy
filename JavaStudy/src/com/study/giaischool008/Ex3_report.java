443ab37bd653f351e20b1ef18e740132baba9aa9
leeky <sleekydz86@icloud.com>
2022-07-01 11:11:36 +0900

    약수 정수집합체

--- JavaStudy/src/com/study/giaischool008/Ex3_report.java
+++ JavaStudy/src/com/study/giaischool008/Ex3_report.java
@@ -15,6 +15,17 @@ public class Ex3_report {
 		  	 6 -> 1+2+3
 		   
 		 */
-		
+		int sum=0;
+		for(int i=1; i<=1000; i++) {
+		  for(int j=1; j<=i; j++) {
+			if(i%j==0) {
+				sum+=j;//약수의 합
+		    }
+		  }
+		  if((sum-i)==i) {//자기자신을 제거...
+			  System.out.println("과제: "+i);
+		  } 
+		  sum=0;
+		}
 	}
 }
