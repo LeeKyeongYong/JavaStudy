6f5966c8e7bfa9fff1efe4052c2101affd90a91e
aischool <aischool@DESKTOP-JKHMJJ5>
2022-07-08 15:18:38 +0900

    메소드 수정

--- JavaStudy/src/com/study/ajaischool/method/Ex01.java
+++ JavaStudy/src/com/study/ajaischool/method/Ex01.java
@@ -8,11 +8,23 @@ public class Ex01 {
 		  함수
 		   
 		 */
-		int num1 = 10;
-		int num2 = 20;
+		int num1 = 3;
+		int num2 = 5;
 		
 		//메소드 실행하다.
-		System.out.println("합: "+add(num1,num2));
+		//메소드 호출 --> 메소드 실행
+		
+		//메소드 전달 변수명과 상관 없이 안에 잇는 값만 넘어간다.
+		int addNum=add(num1,num2);
+		int minusNum=minus(3,5);
+		int mulNum=gob(3,5);
+		int divNum=nanugi(3,5);
+		
+		System.out.println("더하기결과: "+addNum);
+		System.out.println("빼기결과: "+minusNum);
+		System.out.println("곱하기결과: "+mulNum);
+		System.out.println("나누기결과: "+divNum);
+		
 	}
 	//더하기 메소드 작성  부분
 	//public : 접근제한자(모든 접근하게 허용하겠습니다.)
@@ -24,6 +36,21 @@ public class Ex01 {
 	//return : 결과값 반환 명령어
 	public static int add(int num1,int num2) {
 		int result=num1+num2;
+		return result;
+	}
+	
+	public static int minus(int num1,int num2) {
+		int result=num1-num2;
+		return result;
+	}
+	
+	public static int gob(int num1,int num2) {
+		int result=num1*num2;
+		return result;
+	}
+	
+	public static int nanugi(int num1,int num2) {
+		int result=num1/num2;
 		return result;
 	}
 }
