8947ff196a97c913550b15856236578d3bce1d96
aischool <aischool@DESKTOP-JKHMJJ5>
2022-07-12 09:11:55 +0900

    메소드

---
+++ JavaStudy/src/com/study/ajaischool/method/Ex05.java
@@ -0,0 +1,13 @@
+package com.study.ajaischool.method;
+/*
+ Exam054
+ */
+public class Ex05 {
+	public static void main(String[] args) {
+		add(10,15);
+	}
+	public static  void add(int num1, int num2) {
+		int result=num1+num2;
+		System.out.println("결과: "+result);
+	}
+}
